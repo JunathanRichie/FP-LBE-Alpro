2fbde54e810a0ff85fab306beadb21f5c21635be
Gerry Nicholas <gerrynicholas76@gmail.com>
2024-09-18 10:03:51 +0700

    feat: create database schema

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ npm install concurrently
 gak usah di-init lagi, ntar kalo mau jalanin webnya, pake:
 <br>
 npm run dev
+
+tanya ke gerry di .env line 26 27, login info database apa
++ jan lupa 
++ `php artisan make:migrate`
++ `php artisan migrate:fresh --seed`
---
+++ database/migrations/2024_09_18_022623_store.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->integer('id_user')->primary();
+            $table->string('nama_user', 50);
+            $table->string('email', 75);
+            $table->string('no_telp', 14);
+        });
+
+        Schema::create('items', function (Blueprint $table) {
+            $table->char('id_item', 14)->primary();
+            $table->string('nama_item', 100);
+            $table->integer('stock_item');
+            $table->float('harga_item', 8, 2);
+        });
+
+        Schema::create('promo_items', function (Blueprint $table) {
+            $table->integer('id_promo')->primary();
+            $table->boolean('aktif');
+            $table->integer('mn_beli');
+            $table->integer('diskon');
+            $table->integer('gratis_item');
+        });
+
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->integer('id_transaction')->primary();
+            $table->float('total_harga', 8, 2);
+            $table->dateTime('tgl_transaksi');
+    
+            $table->integer('id_user');
+            $table->foreign('id_user')->references('id_user')->on('users');
+            
+            $table->char('id_item', 14);
+            $table->foreign('id_item')->references('id_item')->on('items');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+        Schema::dropIfExists('promo_items');
+        Schema::dropIfExists('items');
+        Schema::dropIfExists('users');
+    }
+};
