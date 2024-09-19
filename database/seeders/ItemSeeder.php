a56609c4d6f54a05964f75936ff53853268e84fa
vinsa61 <vinsarioshen610@gmail.com>
2024-09-19 23:27:27 +0700

    gedagedi error image

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -19,67 +19,63 @@ class ItemSeeder extends Seeder
                 'nama_item' => 'Indomie Goreng',
                 'stock_item' => 100,
                 'harga_item' => 2500,
-                'image' => 'laptop.jpg'
+                'image' => 'indomie-goreng.jpg'
             ],
             [
                 'id_item' => '8 123456 789013',
                 'nama_item' => 'Indomie Kari Ayam',
                 'stock_item' => 100,
                 'harga_item' => 3000,
-                'image' => 'laptop.jpg'
+                'image' => 'indomie-kari-ayam.jpg'
             ],
             [
                 'id_item' => '8 123456 789014',
                 'nama_item' => 'Kopi Kapal Api',
                 'stock_item' => 100,
                 'harga_item' => 5000,
-                'image' => 'laptop.jpg'
+                'image' => 'kopi-kapal-api.jpg'
             ],
             [
                 'id_item' => '8 123456 789015',
                 'nama_item' => 'Teh Sosro',
                 'stock_item' => 100,
                 'harga_item' => 4000,
-<<<<<<< HEAD
-                'image' => 'laptop.jpg'
-=======
-                'image' => 'teh-sosro.png'
->>>>>>> 5bbedb781e1bb2f2cd8d5a89946bb2702f61e304
+                'image' => 'teh-sosro.jpg'
             ],
             [
                 'id_item' => '8 123456 789016',
                 'nama_item' => 'Kacang Dua Kelinci 375gr',
                 'stock_item' => 100,
                 'harga_item' => 15000,
-                'image' => 'laptop.jpg'
+                'image' => 'kacang-dua-kelinci.jpg'
             ],
             [
                 'id_item' => '8 123456 789017',
                 'nama_item' => 'Chitato Rasa Ayam Bakar 68gr',
                 'stock_item' => 100,
                 'harga_item' => 10000,
-                'image' => 'laptop.jpg'
+                'image' => 'chitato-ayam-bakar.jpg'
             ],
             [
                 'id_item' => '8 123456 789018',
                 'nama_item' => 'Ultra Milk Full Cream 1000ml',
                 'stock_item' => 100,
                 'harga_item' => 15000,
-                'image' => 'laptop.jpg'
+                'image' => 'ultra-milk-full-cream.jpg'
             ],
             [
                 'id_item' => '8 123456 789019',
                 'nama_item' => 'Kopi ABC Susu 190ml',
                 'stock_item' => 100,
                 'harga_item' => 6000,
-                'image' => 'laptop.jpg'
+                'image' => 'kopi-abc-susu.jpg'
             ],
             [
                 'id_item' => '8 123456 789020',
                 'nama_item' => 'Glico Biscuit Pocky Box 47gr',
                 'stock_item' => 100,
                 'harga_item' => 9190,
-                'image' => 'laptop.jpg'
+                'image' => 'glico-biscuit-pocky.jpg'
                 // promo beli 2 lebih hemat jadi 14.990
             ],
             [
@@ -87,7 +83,7 @@ class ItemSeeder extends Seeder
                 'nama_item' => 'So Klin Softergent Bubuk Bag 1,5kg',
                 'stock_item' => 100,
                 'harga_item' => 38490,
-                'image' => 'laptop.jpg'
+                'image' => 'so-klin-softergent.jpg'
                 // promo 25% jadi 28.500
             ],
             [
@@ -95,7 +91,7 @@ class ItemSeeder extends Seeder
                 'nama_item' => 'Samyang Mi Goreng 2xSpciy Hot Chicken 140gr',
                 'stock_item' => 100,
                 'harga_item' => 25590,
-                'image' => 'laptop.jpg'
+                'image' => 'samyang-2xspicy.jpg'
                 // beli 2 gratis 1 
             ]
         ]);
